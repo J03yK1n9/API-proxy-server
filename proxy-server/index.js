61d1f22fbfed40f44fef4dc22ddbad6b4e1610ad
J03yK1n9 <joeykingbtc@gmail.com>
2021-10-22 13:37:15 +0300

    incorporated api rate limit & caching

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 const { Router } = require('express')
 require('dotenv').config()
 
@@ -7,8 +8,20 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10 // limit each IP to 100 requests per windowMs
+})
+
+// apply to all request
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Set static folder
+app.use(express.static('public'))
+
 // Routes
-app.use('/api/recipes/v2', require('./routes'))
+app.use('/api', require('./routes'))
 
 // Enable cors
 app.use(cors())
--- proxy-server/routes/index.js
+++ proxy-server/routes/index.js
@@ -2,22 +2,36 @@ const url = require('url')
 const express = require("express");
 const router = express.Router();
 const needle = require("needle");
+const apicache = require('apicache');
 
 // Env vars
 const API_BASE_URL = process.env.API_BASE_URL;
+const API_ACCESS_NAME = process.env.API_ACCESS_NAME;
+const API_ACCESS_VALUE = process.env.API_ACCESS_VALUE;
+const API_ID_NAME = process.env.API_ID_NAME;
+const API_ID_VALUE = process.env.API_ID_VALUE;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-router.get("/", async (req, res) => {
+// Init cache
+let cache = apicache.middleware
+
+router.get("/", cache('24 day'), async (req, res) => {
   try {
     const params = new URLSearchParams({
       ...url.parse(req.url,true).query,
+      [API_ACCESS_NAME]: API_ACCESS_VALUE,
+      [API_ID_NAME]: API_ID_VALUE,
       [API_KEY_NAME]: API_KEY_VALUE,
     });
 
     const apiRes = await needle("get", `${API_BASE_URL}?${params}`);
     const data = apiRes.body;
 
+    // Log the request to the public API
+    if(process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`);
+    }
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error });
